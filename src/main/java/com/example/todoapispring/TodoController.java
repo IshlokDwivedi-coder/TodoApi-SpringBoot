1eb79576913fe46c7d75ba515a7e98f6d3ae784a
Ishlok Dwivedi <ishlokdwivedi1@gmail.com>
2025-09-10 19:49:22 +0530

    Added first basic Api thru SpringBoot

---
+++ src/main/java/com/example/todoapispring/TodoController.java
@@ -0,0 +1,27 @@
+package com.example.todoapispring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TodoController {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Todo> todoList;
+
+    public TodoController(){
+        todoList=new ArrayList<>();
+        todoList.add(new Todo(1,false,"Todo 1",1));
+        todoList.add(new Todo(2 ,true,"Todo 2",2));
+    }
+
+    @GetMapping("/todos")
+    public List<Todo> gettodos(){
+        return todoList;
+    }
+}
